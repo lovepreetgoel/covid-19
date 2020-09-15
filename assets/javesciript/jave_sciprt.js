8c27383570222e0e768fefdef4c3b3dbba562fc8
lovepreetgoel <lovepreet.tr.mdb@gmail.com>
2020-09-15 11:17:27 +0530

    update code

--- assets/javesciript/jave_sciprt.js
+++ assets/javesciript/jave_sciprt.js
@@ -4,7 +4,14 @@
     arrows:false,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
-
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 774,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+    ]
   });
-  
